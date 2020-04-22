59615e1422937490c99474ba1b1adff448a61a36
Markus Tacker <Markus.Tacker@NordicSemi.no>
2020-04-22 15:14:48 +0200

    feat: delete S3 buckets

--- lambda/cleaner.ts
+++ lambda/cleaner.ts
@@ -1,6 +1,7 @@
-import { CloudFormation } from 'aws-sdk'
+import { CloudFormation, S3 } from 'aws-sdk'
 
 const cf = new CloudFormation()
+const s3 = new S3()
 
 const STACK_NAME_REGEX = process.env.STACK_NAME_REGEX
 	? new RegExp(process.env.STACK_NAME_REGEX)
@@ -32,7 +33,7 @@ export const handler = async () => {
 	const ignoredStacks = StackSummaries?.filter(
 		({ StackName }) => !stacksToDelete.includes(StackName),
 	).map(({ StackName }) => StackName)
-	console.log(JSON.stringify({ ignoredStacks }))
+	ignoredStacks?.forEach((name) => console.log(`Ignored: ${name}`))
 
 	// Shuffle the array, this helps to delete stacks which have dependencies to other stacks.
 	// Eventually all stacks will be deleted in the right order
@@ -48,7 +49,44 @@ export const handler = async () => {
 	return stacksToDelete.slice(0, 10).reduce(
 		async (promise, StackName) =>
 			promise.then(async () => {
-				console.log(StackName)
+				// Delete S3 Buckets of the stack
+				const resources = await cf
+					.describeStackResources({ StackName })
+					.promise()
+				const s3buckets = resources?.StackResources?.filter(
+					(res) => res.ResourceType === 'AWS::S3::Bucket',
+				).map(({ PhysicalResourceId }) => PhysicalResourceId as string)
+				await Promise.all(
+					s3buckets?.map(async (Bucket) => {
+						console.log(`Deleting S3 Bucket: ${Bucket}`)
+						// Delete Items
+						s3.listObjects({ Bucket })
+							.promise()
+							.then(async ({ Contents }) => {
+								if (Contents)
+									await s3
+										.deleteObjects({
+											Bucket,
+											Delete: {
+												Objects: Contents.map(({ Key }) => ({
+													Key: Key as string,
+												})),
+											},
+										})
+										.promise()
+								// Delete Bucket
+								return s3.deleteBucket({ Bucket }).promise()
+							})
+							.catch((err) => {
+								console.debug(
+									`Failed to delete bucket ${Bucket}: ${err.message}`,
+								)
+							})
+					}) ?? [],
+				)
+
+				// Delete the Stack itself
+				console.log(`Deleting: ${StackName}`)
 				await cf.deleteStack({ StackName }).promise()
 			}),
 		Promise.resolve(),
