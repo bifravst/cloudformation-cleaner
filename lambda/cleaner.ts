7206602a6e2cdd054940996d208c248941a5b3fd
Markus Tacker <Markus.Tacker@NordicSemi.no>
2021-01-04 16:42:19 +0100

    feat: upgrade to AWS SDK v3
    See https://github.com/bifravst/aws/issues/818
    
    BREAKING CHANGE: Requires use of AWS SDK v3

--- lambda/cleaner.ts
+++ lambda/cleaner.ts
@@ -1,15 +1,27 @@
-import { CloudFormation, S3 } from 'aws-sdk'
+import {
+	DeleteBucketCommand,
+	DeleteObjectsCommand,
+	ListObjectsCommand,
+	S3Client,
+} from '@aws-sdk/client-s3'
+import {
+	CloudFormationClient,
+	DeleteStackCommand,
+	DescribeStackResourcesCommand,
+	ListStacksCommand,
+} from '@aws-sdk/client-cloudformation'
 
-const cf = new CloudFormation()
-const s3 = new S3()
+const cf = new CloudFormationClient({})
+const s3 = new S3Client({})
 
-const STACK_NAME_REGEX = process.env.STACK_NAME_REGEX
-	? new RegExp(process.env.STACK_NAME_REGEX)
-	: /^bifravst-/
+const STACK_NAME_REGEX =
+	process.env.STACK_NAME_REGEX !== undefined
+		? new RegExp(process.env.STACK_NAME_REGEX)
+		: /^bifravst-/
 
-export const handler = async () => {
-	const { StackSummaries } = await cf
-		.listStacks({
+export const handler = async (): Promise<void> => {
+	const { StackSummaries } = await cf.send(
+		new ListStacksCommand({
 			StackStatusFilter: [
 				'CREATE_FAILED',
 				'CREATE_COMPLETE',
@@ -17,15 +29,18 @@ export const handler = async () => {
 				'ROLLBACK_COMPLETE',
 				'DELETE_FAILED',
 			],
-		})
-		.promise()
+		}),
+	)
 
 	// Find stacks that match the given regex in the event and that are older than 24 hours
 	const stacksToDelete =
-		StackSummaries?.filter(({ StackName }) => STACK_NAME_REGEX.test(StackName))
+		StackSummaries?.filter(({ StackName }) =>
+			STACK_NAME_REGEX.test(StackName ?? ''),
+		)
 			.filter(
 				({ CreationTime }) =>
-					Date.now() - CreationTime.getTime() > 24 * 60 * 60 * 1000,
+					Date.now() - (CreationTime?.getTime() ?? Date.now()) >
+					24 * 60 * 60 * 1000,
 			)
 			.map(({ StackName }) => StackName) ?? []
 
@@ -50,9 +65,9 @@ export const handler = async () => {
 		async (promise, StackName) =>
 			promise.then(async () => {
 				// Delete S3 Buckets of the stack
-				const resources = await cf
-					.describeStackResources({ StackName })
-					.promise()
+				const resources = await cf.send(
+					new DescribeStackResourcesCommand({ StackName }),
+				)
 				const s3buckets = resources?.StackResources?.filter(
 					(res) => res.ResourceType === 'AWS::S3::Bucket',
 				).map(({ PhysicalResourceId }) => PhysicalResourceId as string)
@@ -60,22 +75,22 @@ export const handler = async () => {
 					s3buckets?.map(async (Bucket) => {
 						console.log(`Deleting S3 Bucket: ${Bucket}`)
 						// Delete Items
-						s3.listObjects({ Bucket })
-							.promise()
+						s3.send(new ListObjectsCommand({ Bucket }))
 							.then(async ({ Contents }) => {
 								if (Contents)
-									await s3
-										.deleteObjects({
+									await s3.send(
+										new DeleteObjectsCommand({
 											Bucket,
 											Delete: {
 												Objects: Contents.map(({ Key }) => ({
 													Key: Key as string,
 												})),
 											},
-										})
-										.promise()
+										}),
+									)
+
 								// Delete Bucket
-								return s3.deleteBucket({ Bucket }).promise()
+								return s3.send(new DeleteBucketCommand({ Bucket }))
 							})
 							.catch((err) => {
 								console.debug(
@@ -87,7 +102,7 @@ export const handler = async () => {
 
 				// Delete the Stack itself
 				console.log(`Deleting: ${StackName}`)
-				await cf.deleteStack({ StackName }).promise()
+				await cf.send(new DeleteStackCommand({ StackName }))
 			}),
 		Promise.resolve(),
 	)
