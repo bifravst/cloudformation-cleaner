b40e3efc6e250c7738e06c67239f3c93a01e56d5
Markus Tacker <Markus.Tacker@NordicSemi.no>
2021-02-04 09:21:57 +0100

    fix: use Node.js 14

--- cdk/Stack.ts
+++ cdk/Stack.ts
@@ -24,7 +24,10 @@ export class Stack extends CloudFormation.Stack {
 			),
 			description: 'Cleans old CloudFormation stacks',
 			handler: 'index.handler',
-			runtime: Lambda.Runtime.NODEJS_12_X, // NODEJS_14_X does not support inline functions, yet. See https://github.com/aws/aws-cdk/pull/12861#discussion_r570038002,
+			// runtime: Lambda.Runtime.NODEJS_14_X, // FIXME: use once CDK has support
+			runtime: new Lambda.Runtime('nodejs14.x', Lambda.RuntimeFamily.NODEJS, {
+				supportsInlineCode: true,
+			}),
 			timeout: CloudFormation.Duration.seconds(60),
 			initialPolicy: [
 				new IAM.PolicyStatement({
